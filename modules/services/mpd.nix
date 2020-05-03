9e249d83ea8786ef35543d9f19cf730509fd15af
Dusk Banks <me@bb010g.com>
2020-05-02 18:08:07 -0700

    mpd: Refactoring (3/6)

--- modules/services/mpd.nix
+++ modules/services/mpd.nix
@@ -27,18 +27,6 @@ let
 
   mpdOptions = cfg: {
 
-  };
-
-in {
-
-  ###### interface
-
-  options = {
-
-    services.mpd = {
-
-      enable = mkEnableOption "MPD, the music player daemon";
-
       package = mkOption {
         type = types.package;
         default = pkgs.mpd;
@@ -130,6 +118,18 @@ in {
           configuration.
         '';
       };
+
+  };
+
+in {
+
+  ###### interface
+
+  options = {
+
+    services.mpd = {
+
+      enable = mkEnableOption "MPD, the music player daemon";
 
     } // mpdOptions cfg;
 
